24381ac4da859f7fb7120c3a1488f9f6067af22e
yuchanns <airamusume@gmail.com>
2019-07-31 00:42:48 +0800

    implements count-leaves int two way

--- chapter2/interface.scm
+++ chapter2/interface.scm
@@ -13,6 +13,30 @@
 ; another length
 (define (length sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+; horner
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff (* x higher-terms)))
+              0
+              coefficient-sequence))
+; another count-leaves
+(define (count-leaves t)
+  (accumulate +
+              0
+              (map (lambda (sub-tree)
+                     (if (pair? sub-tree)
+                       (count-leaves sub-tree)
+                       1))
+                   t)))
+; third count-leaves
+(define (count-leaves-third t)
+  (accumulate (lambda (x y)
+                (if (not (pair? x))
+                  (+ 1 y)
+                  (+ (count-leaves-third x) y)))
+              0
+              (map (lambda (sub-tree) sub-tree)
+                   t)))
 
 ; test output
 (display "another map: ")
@@ -23,3 +47,12 @@
 (newline)
 (display "another length: ")
 (display (length (list 1 2 3 4 5 6)))
+(newline)
+(display "horner eval: ")
+(display (horner-eval 2 (list 1 3 0 5 0 1)))
+(newline)
+(display "count leaves: ")
+(display (count-leaves (list (list 1 (list 2 3)) (list (list 4 5) 6))))
+(newline)
+(display "count leaves third: ")
+(display (count-leaves-third (list (list 1 (list 2 3)) (list (list 4 5) 6))))

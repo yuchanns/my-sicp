86381af3b04670f9b42701be7e1ef89095aa8abb
yuchanns <airamusume@gmail.com>
2019-09-01 23:03:13 +0800

    add permutations

--- chapter2/flatmap.scm
+++ chapter2/flatmap.scm
@@ -42,4 +42,17 @@
                     (map (lambda (j) (list i j))
                          (enumerate-interval 1 (- i 1))))
                   (enumerate-interval 1 n)))))
-(display (prime-sum-pairs 10))
+; (display (prime-sum-pairs 10))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+                (map (lambda (p) (cons x p))
+                     (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+(display (permutations (list 1 2 3 4)))
